40f9eb35e7c1502074d7f1863221ec51591c9721
ysh209 <1750565128@qq.com>
2025-07-17 15:11:23 +0800

    second commit

---
+++ src/views/404/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>这里是404路由</h1>
+</template>
+<script setup></script>
+<style scoped lang="scss"></style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>这里是home路由</h1>
+</template>
+<script setup></script>
+<style scoped lang="scss"></style>

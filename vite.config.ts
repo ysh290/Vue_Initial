d74f84759eb7ea277b589aa1c55e91337ba7cc5a
ysh209 <1750565128@qq.com>
2025-07-18 16:08:22 +0800

    解决前端请求后端接口的跨域问题

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,20 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  //解决前端请求后端接口的跨域问题
+  server: {
+    proxy: {
+      '/api': {
+        // 目标服务器地址，这里是本地的另一个服务，运行在端口8080上
+        target: 'http://127.0.0.1:8080',
+        // 是否改变请求的源（Origin），设置为true时，会将请求的源修改为目标服务器的源
+        changeOrigin: true,
+        // 路径重写规则，将以/api开头的路径替换为空字符串
+        // 例如，请求/api/users会被转发到目标服务器的/users路径
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
